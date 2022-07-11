6ca8c891510868baa0455777ff5f975ffbfacbd9
Archer Chua <archer@Archers-MacBook-Pro.local>
2022-07-12 02:05:10 +0800

    finished minting and refund feature

--- main.py
+++ main.py
@@ -1,26 +1,72 @@
 from utils import Utils
 from dotenv import load_dotenv
 import os
+import shutil
 
 load_dotenv()
 PAYMENT_ADDR = os.getenv("PAYMENT_ADDR")
 POLICY_ID = os.getenv("POLICY_ID")
 HALLPASS_POLICY_ID = os.getenv("HALLPASS_POLICY_ID")
+PROFIT_ADDR = os.getenv("PROFIT_ADDR")
+TXN_FILE_PATH = "/Users/archer/Documents/HIMA-dev/minter/transactions"
+TEMP_METADATA_PATH = "/Users/archer/Documents/HIMA-dev/minter/temp/temp.json"
+POLICY_SCRIPT_PATH = "/Users/archer/Documents/HIMA-dev/minter/policy/policy.script"
+PAYMENT_SKEY = "/Users/archer/Documents/HIMA-dev/minter/keys/payment.skey"
+POLICY_SKEY = "/Users/archer/Documents/HIMA-dev/minter/policy/policy.skey"
 worker = Utils
 
 # Fetch transactions
 utxos = worker.fetch_utxo(PAYMENT_ADDR)
 # Check transctions
 for utxo in utxos:
-    worker.check_utxo(utxo, 5000000)
+    worker.check_utxo(utxo=utxo, mint_price=5000000)
 
 # Sort transactions by time received
-sorted_utxos = worker.sort_txn(utxos)
+sorted_utxos = worker.sort_txn(txns=utxos)
 
 for utxo in utxos:
     # generate metadata
-    worker.generate_metadata(mint_amount=utxo.mint_amount, policyID=f"{POLICY_ID}")
-    print(f"Generating metadata for {utxo.addr}")
+    if utxo.sellable:
+        metadata_paths = worker.generate_metadata(
+            mint_amount=utxo.mint_amount,
+            policyID=f"{POLICY_ID}",
+            outfile_path=TEMP_METADATA_PATH,
+        )
+        real_token_names = [
+            tokens.split("/", 1)[1].split(".")[0]
+            for tokens in metadata_paths  # Getting file name from path
+        ]
+
+        # Build mint transaction
+        worker.build_mint_txn(
+            policyid=POLICY_ID,
+            utxo=utxo,
+            full_metadata=TEMP_METADATA_PATH,
+            real_token_names=real_token_names,
+            profit_addr=PROFIT_ADDR,
+            script=POLICY_SCRIPT_PATH,
+            outfile=TXN_FILE_PATH,
+        )
+    else:
+        worker.build_refund_txn(utxo=utxo, outfile=TXN_FILE_PATH)
+
+    # Sign transaction
+    worker.sign_txn(
+        sellable=utxo.sellable,
+        policy_skey=POLICY_SKEY,
+        payment_skey=PAYMENT_SKEY,
+        bodyfile=f"{TXN_FILE_PATH}/matx.raw",
+        outfile=TXN_FILE_PATH,
+    )
+
+    # Submit transaction
+    submitted = worker.submit_txn(bodyfile=f"{TXN_FILE_PATH}/matx.signed")
+    if submitted:
+        if utxo.sellable:
+            for metadata in metadata_paths:
+                filename = metadata.split("/", 1)[1]
+                shutil.move(f"./{metadata}", f"./minted/{filename}")
+
 
 # Snapshot holders of hallpass
 # worker.snapshot(HALLPASS_POLICY_ID)
--- utils.py
+++ utils.py
@@ -2,9 +2,16 @@ import requests
 from datetime import datetime
 import os, random, json
 from dotenv import load_dotenv
+import subprocess
 
 load_dotenv()
 CARDANO_CLI_PATH = "cardano-cli"
+
+
+def get_current_slot():
+    response = requests.get("https://api.koios.rest/api/v0/tip").json()
+    slot = int(response[0]["abs_slot"])
+    return slot
 
 
 class Transaction:
@@ -29,6 +36,84 @@ class Transaction:
         self.mint_amount = mint_amount
         self.sellable = sellable
         self.refundable = refundable
+
+
+def build_refund_command(utxo: Transaction, outfile: str, output: int = 1500000):
+    command = [CARDANO_CLI_PATH, "transaction", "build", "--alonzo-era", "--mainnet"]
+    command.append("--tx-in")
+    command.append(f"{utxo.hash}#{utxo.index}")
+    command.append("--tx-out")
+    txn_output = f"{utxo.addr}+{output}"
+    for asset in utxo.assets:
+        txn_output += (
+            f"+ {asset['quantity']} {asset['policy_id']}.{asset['asset_name']}"
+        )
+    command.append(txn_output)
+    command.append("--change-address")
+    command.append(utxo.addr)
+    command.append("--out-file")
+    command.append(f"{outfile}/matx.raw")
+    return command
+
+
+def build_mint_command(
+    policyid: str,
+    utxo: Transaction,
+    full_metadata: str,
+    real_token_names: list,
+    profit_addr: str,
+    script: str,
+    outfile: str,
+    output: int = 1500000,
+):
+    hex_token_names = [
+        real_token_name.encode("utf-8").hex() for real_token_name in real_token_names
+    ]
+    command_draft_txn = [
+        CARDANO_CLI_PATH,
+        "transaction",
+        "build",
+        "--mainnet",
+        "--alonzo-era",
+    ]
+    txn_input = f"{utxo.hash}#{utxo.index}"
+    command_draft_txn.append("--tx-in")
+    command_draft_txn.append(txn_input)
+
+    txn_output = f"{utxo.addr}+{output}"
+    for token in hex_token_names:
+        txn_output += f"+1 {policyid}.{token}"
+    command_draft_txn.append("--tx-out")
+    command_draft_txn.append(txn_output)
+
+    command_draft_txn.append("--change-address")
+    command_draft_txn.append(profit_addr)
+
+    mint = "--mint="
+    for index, token in enumerate(hex_token_names):
+        if index == 0:
+            mint += f"1 {policyid}.{token}"
+        else:
+            mint += f"+ 1 {policyid}.{token}"
+    command_draft_txn.append(mint)
+
+    command_draft_txn.append("--minting-script-file")
+    command_draft_txn.append(script)
+
+    command_draft_txn.append("--metadata-json-file")
+    command_draft_txn.append(full_metadata)
+
+    command_draft_txn.append("--invalid-hereafter")
+    current_slot = get_current_slot()
+    slot = current_slot + 10800  # Current slot + 3hrs
+    command_draft_txn.append(str(slot))
+
+    command_draft_txn.append("--witness-override")
+    command_draft_txn.append("2")
+    command_draft_txn.append("--out-file")
+    command_draft_txn.append(f"{outfile}/matx.raw")
+
+    return command_draft_txn
 
 
 class Utils:
@@ -68,24 +153,19 @@ class Utils:
     def check_utxo(utxo, mint_price: int):
         if utxo.lovelace > 2000000:
             utxo.refundable = True
-            if (
-                utxo.lovelace % mint_price == 0
-                and utxo.lovelace / mint_price != 0
-                and utxo.lovelace / mint_price <= 10
-            ):
-                amount = int(utxo.lovelace / mint_price)
-                if amount <= len(
-                    os.listdir("/Users/archer/Documents/HIMA-dev/minter/metadata")
+            if len(utxo.assets) == 0:
+                if (
+                    utxo.lovelace % mint_price == 0
+                    and utxo.lovelace / mint_price != 0
+                    and utxo.lovelace / mint_price <= 10
                 ):
-                    utxo.mint_amount = amount
-                    utxo.sellable = True
-                else:
-                    utxo.sellable = False
-            else:
-                utxo.sellable = False
-        else:
-            utxo.sellable = False
-            utxo.refundable = False
+                    amount = int(utxo.lovelace / mint_price)
+                    if amount <= len(
+                        os.listdir("/Users/archer/Documents/HIMA-dev/minter/metadata")
+                    ):
+                        utxo.mint_amount = amount
+                        utxo.sellable = True
+
         print(
             f"hash: {utxo.hash}#{utxo.index}\naddr: {utxo.addr}\nmint amount: {utxo.mint_amount}\nsellable: {utxo.sellable}\nrefundable: {utxo.refundable}"
         )
@@ -101,44 +181,141 @@ class Utils:
         print(f"{len(txns)} transactions sorted.")
         return sorted_txns
 
-    def generate_metadata(policyID: str, mint_amount: int):
+    def generate_metadata(policyID: str, mint_amount: int, outfile_path: str):
         template_top = '{"721": {"' + policyID + '": {'
         template_bottom = "}}}"
+
         metadata_paths = []
         for x in range(mint_amount):
-            metadata_paths.append(
-                "metadata/"
-                + random.choice(
+            metadata = random.choice(
+                os.listdir("/Users/archer/Documents/HIMA-dev/minter/metadata")
+            )
+            while f"metadata/{metadata}" in metadata_paths:
+                metadata = random.choice(
                     os.listdir("/Users/archer/Documents/HIMA-dev/minter/metadata")
                 )
-            )
+            metadata_paths.append("metadata/" + metadata)
 
         metadatas = []
-        for path in metadata_paths:
-            with open(path) as metadata:
+        for metadata_path in metadata_paths:
+            with open(metadata_path) as metadata:
                 metadatas.append(str(json.load(metadata))[1:-1])
         joined_metadata = ",".join(metadatas)
         final_metadata_str = f"{template_top}{joined_metadata}{template_bottom}"
-        with open("./temp/temp.json", "w") as outfile:
+        with open(outfile_path, "w") as outfile:
             outfile.write(final_metadata_str.replace("'", '"'))
-        print(f"Metadata generated.\npath: ./temp/temp.json")
+        print(f"Metadata generated.\npath: {outfile_path}")
+        return metadata_paths
 
-    # TODO
-    def build_mint_command(policyid: str, utxo: Transaction, fee: int = 1500000):
-        command_draft_txn = [
+    def build_refund_txn(
+        utxo: Transaction,
+        outfile: str,
+    ):
+        command = build_refund_command(utxo=utxo, outfile=outfile)
+        draft_txn = subprocess.Popen(command, stdout=subprocess.PIPE)
+        output, error = draft_txn.communicate()
+        if error:
+            print("Error: ", error)
+        fee = str(output).split("Lovelace ", 1)[1].split("\\", 1)[0]
+        fee = 1500000 + int(fee)
+        command = build_refund_command(utxo=utxo, outfile=outfile, output=fee)
+        subprocess.Popen(command)
+
+    def build_mint_txn(
+        policyid: str,
+        utxo: Transaction,
+        full_metadata: str,
+        real_token_names: list,
+        profit_addr: str,
+        script: str,
+        outfile: str,
+    ):
+        command = build_mint_command(
+            policyid=policyid,
+            utxo=utxo,
+            full_metadata=full_metadata,
+            real_token_names=real_token_names,
+            profit_addr=profit_addr,
+            script=script,
+            outfile=outfile,
+        )
+        draft_txn = subprocess.Popen(command, stdout=subprocess.PIPE)
+        output, error = draft_txn.communicate()
+        if error:
+            print("Error: ", error)
+        fee = (
+            str(output).split("Lovelace ", 1)[1].split("\\", 1)[0]
+        )  # Get fee from terminal output
+        command = build_mint_command(
+            policyid=policyid,
+            utxo=utxo,
+            full_metadata=full_metadata,
+            real_token_names=real_token_names,
+            profit_addr=profit_addr,
+            script=script,
+            outfile=outfile,
+            output=int(fee) + (utxo.mint_amount * 1500000),
+        )
+        subprocess.Popen(command)
+
+    def sign_txn(
+        sellable: bool, policy_skey: str, payment_skey: str, bodyfile: str, outfile: str
+    ):
+        if sellable:
+            print("Signing mint txn")
+            command = [
+                CARDANO_CLI_PATH,
+                "transaction",
+                "sign",
+                "--signing-key-file",
+                f"{payment_skey}",
+                "--signing-key-file",
+                f"{policy_skey}",
+                "--mainnet",
+                "--tx-body-file",
+                f"{bodyfile}",
+                "--out-file",
+                f"{outfile}/matx.signed",
+            ]
+            subprocess.run(command)
+        else:
+            print("Signing refund txn")
+            command = [
+                CARDANO_CLI_PATH,
+                "transaction",
+                "sign",
+                "--signing-key-file",
+                f"{payment_skey}",
+                "--mainnet",
+                "--tx-body-file",
+                f"{bodyfile}",
+                "--out-file",
+                f"{outfile}/matx.signed",
+            ]
+            subprocess.run(command)
+
+    def submit_txn(bodyfile: str):
+        print("submitting txn")
+        command = [
             CARDANO_CLI_PATH,
             "transaction",
-            "build",
+            "submit",
+            "--tx-file",
+            f"{bodyfile}",
             "--mainnet",
-            "--alonzo-era",
         ]
-        txn_input = f"{utxo.hash}#{utxo.index}"
-        command_draft_txn.append("--tx-in")
-        command_draft_txn.append(txn_input)
-
-        txn_output = f"{utxo.addr}+{fee}"
-        for x in range(utxo.mint_amount):
-            txn_output += f"+1{policyid}."
+        submit = subprocess.Popen(command, stdout=subprocess.PIPE)
+        output, error = submit.communicate()
+        if error:
+            print("Error: ", error)
+            return False
+        output = str(output)
+        if (
+            output.split("'", 1)[1].split(".", 1)[0]
+            != "Transaction successfully submitted"
+        ):
+            return False
+        return True
 
     def snapshot(policyid: str):
         response = requests.get(
